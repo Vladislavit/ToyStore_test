45f7224be16c28139900330b04670a0400e0ecce
Vladislavit <zvolinskiy.ukr.net@gmail.com>
2024-04-16 21:52:34 +0300

    Created views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+def card(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+def checkout(request):
+    context = {}
+    return render(request, 'store/store.html', context)
