8bde9defb55a4d62fd12fd315e26eeee04dfea9b
Vladislavit <zvolinskiy.ukr.net@gmail.com>
2024-04-16 22:03:44 +0300

    fixed faults

--- store/views.py
+++ store/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 def store(request):
     context = {}
     return render(request, 'store/store.html', context)
-def card(request):
+def cart(request):
     context = {}
-    return render(request, 'store/store.html', context)
+    return render(request, 'store/cart.html', context)
 def checkout(request):
     context = {}
-    return render(request, 'store/store.html', context)
+    return render(request, 'store/checkout.html', context)
